cd6f61410393229d85f984c5a21ed73045afb3f4
Antony Tkachov <korvinfromua@gmail.com>
2019-06-20 17:06:58 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import time
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    out = "<h2>Family album Khudich.</h2><br /><P>"
+    out += str( time.time() )
+    out += "</P>"
+    return out
+
+@app.route('/blog')
+def blog():
+    return "<h2>My blog.</h2>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
