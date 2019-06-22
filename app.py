e2375726767260a0fd8bf4bd9104d93c2babb8e5
Antony Tkachov <korvinfromua@gmail.com>
2019-06-22 21:47:56 +0300

    Commit

--- app.py
+++ app.py
@@ -12,15 +12,15 @@ def index():
     a = 4
     b = 5
     c = a * b
-     # out = '<!DOCTYPE html><html><head><meta charset="utf-8">' \
-     #       '<meta name="viewport" content="width=device-width, initial-scale=1"><title>Hello Bulma!</title>' \
-     #       '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">' \
-     #       '<script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script></head><body>' \
-     #       '<section class="section"><div class="container"><h1 class="title">'
-     # out += 'Hello guest</h1><p class="button is-primary">'
-     # out += str( time.time() )
-     # out += "</p></div></section></body></html>"
-     return c
+    # out = '<!DOCTYPE html><html><head><meta charset="utf-8">' \
+    #       '<meta name="viewport" content="width=device-width, initial-scale=1"><title>Hello Bulma!</title>' \
+    #       '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">' \
+    #       '<script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script></head><body>' \
+    #       '<section class="section"><div class="container"><h1 class="title">'
+    # out += 'Hello guest</h1><p class="button is-primary">'
+    # out += str( time.time() )
+    # out += "</p></div></section></body></html>"
+    return c
 
 @app.route('/blog')
 def blog():
